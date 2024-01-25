6e8afd0766308a4d5adea38e75dee5b65bfb48a4
Muhamed Neziri <mn30176@seeu.edu.mk>
2024-01-25 15:59:30 +0100

    innit

---
+++ src/main/java/com/example/javaPharma/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.example.javaPharma.service;
+
+
+import com.example.javaPharma.pojo.dto.CreateCategoryRequest;
+import com.example.javaPharma.pojo.entity.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+
+    Category getCategoryById(Long id);
+
+    Category saveCategory(CreateCategoryRequest category);
+
+    void deleteCategory(Long id);
+}
