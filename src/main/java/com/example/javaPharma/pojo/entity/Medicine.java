0fc0f854f5aad3d2867e845230d901dcfc57501d
Muhamed Neziri <mn30176@seeu.edu.mk>
2024-01-25 20:36:49 +0100

    Medicine Entity Getters Setters

--- src/main/java/com/example/javaPharma/pojo/entity/Medicine.java
+++ src/main/java/com/example/javaPharma/pojo/entity/Medicine.java
@@ -26,4 +26,52 @@ public class Medicine {
     @ManyToOne
     @JoinColumn(name = "expiry_date_id")
     private ExpiryDate expiryDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public DateCreated getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(DateCreated dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public ExpiryDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(ExpiryDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
