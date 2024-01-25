9066266c8130e20332bf8e9b6ecc3559045bb9fe
Muhamed Neziri <mn30176@seeu.edu.mk>
2024-01-25 21:18:45 +0100

    CategoryController

--- src/main/java/com/example/javaPharma/controller/CategoryController.java
+++ src/main/java/com/example/javaPharma/controller/CategoryController.java
@@ -1,4 +1,44 @@
 package com.example.javaPharma.controller;
 
+
+import com.example.javaPharma.pojo.dto.CreateCategoryRequest;
+import com.example.javaPharma.pojo.entity.Category;
+import com.example.javaPharma.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody CreateCategoryRequest category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody CreateCategoryRequest category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
 }
+
