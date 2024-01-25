4a8f65610512163572f462bbb78427f1fa621e3b
Muhamed Neziri <mn30176@seeu.edu.mk>
2024-01-25 21:09:11 +0100

    ManufcaturerService IMPL

---
+++ src/main/java/com/example/javaPharma/service/ManufacturerServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.javaPharma.service;
+
+
+import com.example.javaPharma.pojo.entity.Manufacturer;
+import com.example.javaPharma.repository.ManufacturerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ManufacturerServiceImpl implements ManufacturerService {
+
+    @Autowired
+    private ManufacturerRepository manufacturerRepository;
+
+    @Override
+    public List<Manufacturer> getAllManufacturers() {
+        return manufacturerRepository.findAll();
+    }
+
+    @Override
+    public Manufacturer getManufacturerById(Long id) {
+        return manufacturerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Manufacturer saveManufacturer(Manufacturer manufacturer) {
+        return manufacturerRepository.save(manufacturer);
+    }
+
+    @Override
+    public void deleteManufacturer(Long id) {
+        manufacturerRepository.deleteById(id);
+    }
+}
