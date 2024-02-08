9d7c2489291d6394aef455ed1a1bb04f2685940e
Muhamed Neziri <mn30176@seeu.edu.mk>
2024-02-08 22:38:12 +0100

    delete

--- src/main/java/com/example/javaPharma/repository/CategoryRepository.java
+++ src/main/java/com/example/javaPharma/repository/CategoryRepository.java
@@ -5,5 +5,5 @@ import com.example.javaPharma.pojo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    // Add custom queries if needed
+
 }
--- src/main/java/com/example/javaPharma/repository/DateCreatedRepository.java
+++ src/main/java/com/example/javaPharma/repository/DateCreatedRepository.java
@@ -5,5 +5,5 @@ import com.example.javaPharma.pojo.entity.DateCreated;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DateCreatedRepository extends JpaRepository<DateCreated, Long> {
-    // Add custom queries if needed
+
 }
